4b1f28738fe271206d46867057632be9e847c337
Ngoc Anh <ngocanha85@gmail.com>
2023-02-10 10:06:27 +0700

    return to 8a62144b

---
+++ src/exercise/Lab101.java
@@ -0,0 +1,15 @@
+package exercise;
+
+/**
+ * @author AnhNN66
+ */
+public class Lab101 {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        int number1 = 100;
+        int number2 = 200;
+        System.out.println("Sum: " + (number1 + number2));
+    }
+}
